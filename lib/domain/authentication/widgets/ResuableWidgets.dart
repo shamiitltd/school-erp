b1cda8c4ab9ab19ea8f21213ebc5d7d48ab4bffc
SHAMIIT <shamiitltd@gmail.com>
2023-02-28 20:23:30 +0530

    //Colors add purple and blue // edit LoginWidget.dart //changes complete //edit ForgetPassword File

--- lib/domain/authentication/widgets/ResuableWidgets.dart
+++ lib/domain/authentication/widgets/ResuableWidgets.dart
@@ -8,33 +8,3 @@ Image logoWidget(String imageName){
     height: 100,
   );
 }
-Container SignInSignUpButton(BuildContext context, bool isLogin, Function onTap) {
-  return Container(
-    width: MediaQuery.of(context).size.width,
-    height: 50,
-    margin: const EdgeInsets.fromLTRB(0, 10, 0, 20),
-    decoration: BoxDecoration(borderRadius: BorderRadius.circular(90)),
-    child: ElevatedButton(
-      onPressed: () {
-        onTap();
-      },
-      style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.resolveWith((states) {
-            if (states.contains(MaterialState.pressed)){
-              return Colors.black26;
-            }
-            return Colors.white;
-          }),
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)))
-      ),
-      child:  Text(
-        isLogin ? 'Log In' : 'Sign Up',
-        style: const TextStyle(color: Colors.black87, fontWeight: FontWeight.bold, fontSize: 16),
-      ),
-
-
-
-    ),
-  );
-}
